3e726cd67908a4d5c9585c8bb5e5a9892f557a68
Seanushka <Sean.nechaev@gmail.com>
2023-11-01 09:50:30 +0200

    Remove junk code

--- Assets/Scripts/PieceMove.cs
+++ Assets/Scripts/PieceMove.cs
@@ -11,27 +11,8 @@ public class PieceMove : MonoBehaviour
     float fallAmount = 0.5f;
     Vector3 piecePos;
 
-    public GameObject scoreManager;
-    float speedLV1;
-    float speedLV2;
-    float speedLV3;
-    float speedLV4;
-
     void Start()
     {
-        scoreManager = GameObject.Find("ScoreManager");
-        speedLV1 = scoreManager.GetComponent<Score>().speed10;
-        speedLV2 = scoreManager.GetComponent<Score>().speed25;
-        speedLV3 = scoreManager.GetComponent<Score>().speed50;
-        speedLV4 = scoreManager.GetComponent<Score>().speed100;
-        Debug.Log("start " + timer);
-        
-        if (scoreManager.GetComponent<Score>().scoreAmount == 10)
-        {
-            timer = timer * speedLV1;
-            Debug.Log(timer);
-        }
-
         timerReset = timer;
     }
 
@@ -46,13 +27,5 @@ public class PieceMove : MonoBehaviour
             this.gameObject.transform.position = piecePos;
             timer = timerReset;
         }
-
-        if (scoreManager.GetComponent<Score>().scoreAmount == 10)
-        {
-            timerReset = timerReset * speedLV1;
-            timer = timer * speedLV1;
-            speedLV1 = 1f;
-        }
-
     }
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -10,47 +10,14 @@ public class Score : MonoBehaviour
     public TextMeshProUGUI score;
     public int scoreAmount;
 
-    public GameObject Spawner;
-    float spawnerTimer;
-
-
-    public float speed10 = 0.75f;
-    public float speed25 = 0.75f;
-    public float speed50 = 0.75f;
-    public float speed100 = 0.75f;
-
-
-
     void Start()
     {
         scoreAmount = 0;
-        spawnerTimer = Spawner.GetComponent<Spawner>().timerReset; // timerReset = 3f In Spawner script
     }
 
     void Update()
     {
         score.text = scoreAmount.ToString();
-
-        if (scoreAmount == 10)
-        {
-            Spawner.GetComponent<Spawner>().timerReset = spawnerTimer * speed10; //timerreset = 3f * 0.75f = 2.25f
-        }
-
-        if (scoreAmount == 25)
-        {
-            Spawner.GetComponent<Spawner>().timerReset = spawnerTimer * speed25; //timerreset = 3f * 0.75f = 2.25f
-        }
-
-        if (scoreAmount == 50)
-        {
-            Spawner.GetComponent<Spawner>().timerReset = spawnerTimer * speed50; //timerreset = 3f * 0.75f = 2.25f
-        }
-
-        if (scoreAmount == 100)
-        {
-            Spawner.GetComponent<Spawner>().timerReset = spawnerTimer * speed100; //timerreset = 3f * 0.75f = 2.25f
-        }
-
     }
 
     public void Addscore()
